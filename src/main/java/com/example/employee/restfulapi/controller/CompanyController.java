993da278a6207d930c52a4e00e77e63b46cad396
zengxiu <1374040664@qq.com>
2018-05-12 00:27:21 +0800

    get companies

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,5 +1,26 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/companies")
 public class CompanyController {
     //在此处完成Company API
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Company> getCompanys(){
+        return companyRepository.findAll();
+    }
 }
