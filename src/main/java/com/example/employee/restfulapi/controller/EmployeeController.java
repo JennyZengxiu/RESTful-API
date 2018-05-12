ab2e6bcae11de3973a6f1f73617977446a1ddd68
zengxiu <1374040664@qq.com>
2018-05-12 13:18:00 +0800

    get employees

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,24 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
     //在此处完成Employee API
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //获取employee列表
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Employee> getEmployees(){
+        return employeeRepository.findAll();
+    }
 }
--- src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
@@ -4,6 +4,12 @@ import com.example.employee.restfulapi.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    //获取employee列表
+
+    @Override
+    List<Employee> findAll();
 }
